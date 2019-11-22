4a54257d920e46bf82325ae6d36acd279cc3cb44
Dilbazsuleyman <56804853+Dilbazsuleyman@users.noreply.github.com>
2019-11-21 19:43:19 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String s1="Hello";
+	if(s1.length()==5){
+        System.out.println("its have enough length"); }
+	 if(s1.contains("o")){
+        System.out.println("it has o");}
+            else{
+            System.out.println("ignore this");}
+
+
+
+    }
+
+
+    }
+
